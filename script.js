81de4bf97ca59c0ee40d411f95f10bb40e104cda
RegmiS <sandeshreg35@gmail.com>
2024-09-18 18:17:36 -0500

    Add newer Book function

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
-        let str = this.read ? "read" : "not read yet";
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${str}`;
-    }
-}
-
-function addBookToLibrary() {
-    // do stuff here
-}
-
-
-const hobbit = new Book("hobbit", "tolkien", 400, false);
-console.log(hobbit.info())
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook)
+};
 
 // function that loops through and displays each book (table/card)
 // new book button with forms. forms in sidebar? dialogs/modals?
 // button to remove book from library
 // button on book to change display and change it's "read" status
+
+function Book (title, author, pages, read) {
+    const wasRead = () => read=true;
+    const notRead = () => read=false;
+    const info = () => {
+        let str = read ? "read" : "not read yet";
+        return `${title} by ${author}, ${pages} pages, ${str}`;
+    };
+    return { title, author, pages, read, info, wasRead, notRead };
+};
+
+const hobbit = Book("hobbit", "tolkien", 400, false);
+const lotr = Book("lotr", "toklien", 500, "true");
+console.log(hobbit);
+console.log(lotr);
