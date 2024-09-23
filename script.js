9cb33e7977602c399f9a083a8ccbe42ed56d697c
RegmiS <sandeshreg35@gmail.com>
2024-09-23 15:03:07 -0500

    Add button to remove from array

--- script.js
+++ script.js
@@ -21,12 +21,26 @@ addBookToLibrary(lotr);
 
 const bookList = document.querySelector("#book-list");
 
+function removeBook(book){
+    let index = myLibrary.indexOf(book);
+    myLibrary.splice(index, 1);
+    displayBooks();
+}
+
 function displayBooks(){
     bookList.innerHTML = "";
     for (let x = 0; x < myLibrary.length; x++){
         const eachBook = myLibrary[x];
         const content = document.createElement('li');
         content.textContent = eachBook.info();
+
+        const button = document.createElement('button');
+        button.innerHTML = "Remove";
+        button.addEventListener('click', () => {
+            removeBook(eachBook);
+        });
+        
+        content.appendChild(button);
         bookList.appendChild(content);
     }
 }
@@ -38,6 +52,7 @@ const bookLength = document.getElementById('booklength');
 
 const btn = document.querySelector('#submit-btn');
 btn.addEventListener("click", btnClick, false);
+
 function btnClick(event) {
     if (bookName.checkValidity() && bookAuthor.checkValidity() && readOrNot.checkVisibility() && bookLength.checkValidity()){
         let name = bookName.value;
@@ -51,7 +66,6 @@ function btnClick(event) {
         bookAuthor.value = "";
         readOrNot.value = false;
         bookLength.value = "";
-
     }
     event.preventDefault();
 }
